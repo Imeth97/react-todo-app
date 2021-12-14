ca2fb4194513edc089024f34a07a33c1413bd3c8
Imeth Don <imethdon@gmail.com>
2021-12-14 16:05:08 +1100

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+
+            
+            <h1>TO-DO</h1>
+            <a href="/">New List</a>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+const Title = () => {
+    const listName = 'To Do List 1'
+    return ( 
+        <div className="Title">
+            <h1>{ listName }</h1>
+        </div>
+     );
+}
+ 
+export default Title;
